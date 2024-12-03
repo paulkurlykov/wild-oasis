4923e514aaa7083e3d887476a561cabee32fddc0
Павел Курлыков <paulkurlykov@gmail.com>
2024-12-03 23:20:11 +0300

    fix vite env locally

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabaseUrl = 'https://qorowfvltclkbddllypn.supabase.co';
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
