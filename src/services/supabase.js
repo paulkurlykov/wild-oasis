3a398b8f34bd5163839c08261c72ef5dcac11117
Павел Курлыков <paulkurlykov@gmail.com>
2024-12-03 23:03:04 +0300

    fix env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabaseUrl = 'https://qorowfvltclkbddllypn.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcm93ZnZsdGNsa2JkZGxseXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwMjE5MTAsImV4cCI6MjA0NDU5NzkxMH0.295tB7YS4D6GeuSvCFVietnhhUVESswIqYV_P9au-8k";
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
